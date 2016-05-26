6d4748f8bb96876b5c874f033fdb329c7be946c1
lzcn <luzhi92@gmail.com>
2016-05-26 23:55:21 +0800

    -

--- projectSampling/src/MatrixMul/wedge_matrix.cpp
+++ projectSampling/src/MatrixMul/wedge_matrix.cpp
@@ -1,9 +1,10 @@
 /*
-	Diamond Sapmling with N factor matrixes
-	It will return a sparse tansor stored 
-	the sampled result
+	Diamond Sampling for matrix multiplication
+	Author: Zhi Lu
+	Reference:"Diamond Sampling for Approximate Maximum 
+			All-pairs Dot-product(MAD) Search"
 */
-
+#include <utility>
 #include <vector>
 #include <map>
 #include <algorithm>
@@ -11,204 +12,392 @@
 #include <cmath>
 #include <ctime>
 #include "mex.h"
-#include "tensor.hpp"
 
-typedef std::vector<size_t> coordinate;
-typedef std::pair<coordinate,double> PAIR;
+struct indexIJ
+{	
+	size_t indI;
+	size_t indJ;
+	indexIJ(size_t i,size_t j){
+		indI = i;
+		indJ = j;
+	}
+	~indexIJ(){}
+	bool operator<(const struct indexIJ &toCmp)const{
+		if(indI < toCmp.indI){
+			return true;
+		}else if(indI == toCmp.indI){
+			if(indJ < toCmp.indJ)
+				return true;
+		}
+		return false;
+	}
+	bool operator==(const struct indexIJ &toCmp){
+		if(indI == toCmp.indI && indJ == toCmp.indJ)
+			return true;
+		return false;
+	}
 
-int cmp(const PAIR &x,const PAIR&y){
+	bool operator > (const struct indexIJ &toCmp){
+		if(indI > toCmp.indI){
+			return true;
+		}else if(indI == toCmp.indI){
+			if(indJ > toCmp.indJ)
+				return true;
+		}
+		return false;
+	}
+};
+
+typedef std::pair<struct indexIJ,double> indValue;
+struct Matrix
+{
+	size_t row;
+	size_t col;
+	double* element;
+	double* SumofCol;
+	Matrix(size_t r, size_t c, double*pr){
+		row = r;
+		col = c;
+		element = pr;
+		SumofCol = (double*)malloc(col*sizeof(double));
+		memset(SumofCol, 0, col*sizeof(double));
+		//get the absolute sum of each columns
+		double temp = 0;
+		for(size_t i = 0; i < col; ++i){
+			temp = 0;
+			for(size_t j = 0; j < row; ++j){
+				temp += abs(element[i*row + j]);
+			}
+			SumofCol[i] = temp;
+		}
+	}
+	~Matrix(){
+		free(SumofCol);
+	}
+	double GetEmelent(size_t i, size_t j){
+		return element[j*row + i];
+	}
+	double GetColSum(size_t column){
+		return SumofCol[column];
+	}
+	size_t randRow(size_t n){
+		double x,sum,temp;
+		sum = SumofCol[n];
+		x = sum*((double)rand()/(double)RAND_MAX);
+		temp = 0;
+		for (size_t i = 0; i < row; ++i){
+			temp += abs(element[i + n*row]);
+			if(x <= temp){ 
+				return i;
+			}
+		}
+		return (row-1);
+	}
+};
+
+
+
+int cmp(const indValue &x,const indValue&y){
 	return x.second > y.second;
 }
 
 int sgn_foo(double x){
 	return x<0? -1:1;
 }
+
 /*
-	give an coord(i_0,i_1,i_2,...,i_(N-1))
-	conpute the value of U_{(i_0,i_1,i_2,...,i_(N-1))}
+	give an pair(i, j)
+	compute the value of c_ij;
 */
-double vectors_mul(const coordinate &coord,\
- 				std::vector<FactorMatrix*> &vMatrixs,\
- 				size_t nrhs,size_t rank_size){
+double vectors_mul(const struct indexIJ &coord, struct Matrix &A, struct Matrix &B){
     double ans = 0;
-    double* temp = new double[rank_size];
-    /*initialize to the column of vMatrixs[0] */
-    for (size_t i = 0; i < rank_size; ++i){
-        temp[i] = vMatrixs[0]->get_element(i,coord[0]);
+    size_t indI = coord.indI;
+    size_t indJ = coord.indJ;
+    for (size_t k = 0; k < A.row; ++k){
+        ans += A.GetEmelent(k,indI) * B.GetEmelent(indJ,k);
     }
-    for (size_t i = 1; i < nrhs; ++i){
-        for(size_t j = 0; j < rank_size; ++j){
-            temp[j] *= vMatrixs[i]->get_element(coord[i],j);
-        }
-    }
-    for (size_t i = 0; i < rank_size; ++i){
-        ans += temp[i];
-    }
-    delete []temp;
     return ans;
 }
-void display(std::vector<size_t> v){
-	std::vector<size_t>::iterator itr;
-	for (itr = v.begin(); itr != v.end(); ++itr){
-		printf("%d, ",(*itr));
-	}
-}
-void display_map(std::map<std::vector<size_t>,double> v){
-	std::map<std::vector<size_t>,double> ::const_iterator itr;
-	for (itr = v.begin(); itr != v.end(); ++itr){
-		display(itr->first);
-		printf("%f\n",itr->second);
-	}
-}
+
+/* 
+	sample the pair(k, i)
+	S: times needed to be sampled;
+	index: will return the result index = i * row + k,
+		it is sorted according to i then k;
+	dim: the length of freq_k;
+	freq_k: will return the number of each k has been sampled;
+	L: the length of the pdf;
+	sum_pdf: the sum of the pdf, which is not one, for the decease
+		of computation cost.
+*/
+
+int sample_index(size_t S, size_t *index, \
+				 size_t dim, int *freq_k,\
+				 size_t L, double*pdf, double sum_pdf);
+
+/* 
+	Vose's alias method for sample;
+	It is used for the situation when 
+		L is not much bigger than S;
+	A method with constant time per sample.
+*/
+int vose_alias(size_t s, size_t *dst, \
+			   size_t n, double *pdf,double sum_pdf);
+/*
+	C = AB;
+	A has d rows and m columns,
+	B has n rows and d columns;
+	It is needed for data structure;
+	Matlab function:
+		[result, index] = function_name(A,B,,top-t,sample_number);
+		result: the desired top-t maximums;
+		index: the corresponding instances from A and B;
+		sample_number = S: control the numbers to do sampling .
+
+*/
 
 void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
+
 	clock_t start,finish;
 	double duration;
 	srand(unsigned(time(NULL)));
-	/* number of samples */
-	const size_t num_sample = 1000000;
-	/* initialization */
 	start = clock();
-	std::vector<FactorMatrix*> vMatrixs;
-	for (int i = 0; i < nrhs; ++i){
-		vMatrixs.push_back(new FactorMatrix(prhs[i]));
-	}
+	//--------------------
+	// Initialization
+	//--------------------
+	struct Matrix MatA(mxGetM(prhs[0]),mxGetN(prhs[0]),mxGetPr(prhs[0]));
+	struct Matrix MatB(mxGetM(prhs[1]),mxGetN(prhs[1]),mxGetPr(prhs[1]));
+	const int top_t = (int)mxGetPr(prhs[2])[0];
+	const size_t NumSample = (size_t)mxGetPr(prhs[3])[0];
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
-	printf("%f seconds during initialization\n",duration);	
+	printf("%f seconds during initialization\n",duration);
+
+	//-------------------------------------
+	// Compute weight
+	//-------------------------------------
+
+	double SumofW = 0;
+	double *weight = (double*)malloc(MatA.row*MatA.col*sizeof(double));
+	memset(weight, 0, MatA.row*MatA.col*sizeof(double));
+	double tempW = 0;
 	start = clock();
-	/* start with matrix:prhs[0] */
-	size_t RANK,L_HEAD;
-	RANK = mxGetM(prhs[0]);
-	L_HEAD = mxGetN(prhs[0]);
-	/* step 1: Get the weight matrix*/
-	double sum_wpr = 0;
-	double *weight = new double[RANK*L_HEAD];
-	double w;
-	start = clock();
-	for (size_t k = 0; k < RANK; ++k){
-		for(size_t i = 0; i < L_HEAD; ++i){
-			w = 1;
-			w *= abs(vMatrixs[0]->get_element(k,i));
-			w *= vMatrixs[0]->sum_of_col()[i];
-			for (size_t j = 1; j < nrhs; ++j){
-				w *= vMatrixs[j]->sum_of_col()[k];
-			}
-			weight[i+k*L_HEAD] = w;
-			sum_wpr += w;
+	for (size_t k = 0; k < MatA.row; ++k){
+		for(size_t i = 0; i < MatA.col; ++i){
+			tempW = 1;
+			tempW *= abs(MatA.GetEmelent(k,i));
+			tempW *= MatA.SumofCol[i];
+			tempW *= MatB.SumofCol[k];
+			weight[i*MatA.col + k] = tempW;
+			SumofW += tempW;
 		}
 	}
 
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
 	printf("%f seconds during computing weight\n",duration);
-	/* Tensor to save samping result */
-	//std::map<coordinate,double> Tensor;
-	std::map<coordinate,double> Tensor;
-	coordinate coords[num_sample];
+
+	//-------------------------
+	// Do Sampling
+	//-------------------------
 
 	start = clock();
-	size_t *index_weight = new size_t[num_sample];
-	std::map<size_t, size_t> freq_k;
-	sample_index(num_sample, index_weight, L_HEAD, freq_k, RANK*L_HEAD, weight, sum_wpr);
-	//vose_alias(num_sample, index_weight, M*N, weight, sum_wpr);
-	size_t index, i_0, i_n, k, kp;
-	int sgn = 1;
-	double value_of_sampled;
-	// push back all i_0
-	for (int s = 0; s < num_sample; ++s){
-		index = index_weight[s];
-		i_0 = index % L_HEAD;		
-		coords[s].push_back(i_0);
+	size_t *WeightInd = (size_t *)malloc(NumSample*sizeof(size_t));
+	memset(WeightInd, 0, NumSample*sizeof(size_t));
+	int *freq_k = (int*)malloc(MatA.row*sizeof(int));
+	memset(freq_k, 0, MatA.row*sizeof(int));
+	// sample S pairs (k, i) ,
+	sample_index(NumSample, WeightInd, \
+				 MatA.row, freq_k,\
+				 MatA.row*MatA.col, weight, SumofW);
+	// k, i, j, k'
+	size_t *IndforI = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IndforI, 0, NumSample*sizeof(size_t));
+	size_t *IndforJ = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IndforJ, 0, NumSample*sizeof(size_t));
+	size_t *IndforKp = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IndforKp, 0, NumSample*sizeof(size_t));
+	// record all i = index / row; index = i * row + k;
+	// and sample k';
+	for (int s = 0; s < NumSample; ++s){
+		IndforI[s] = WeightInd[s] / MatA.row;
+		IndforKp[s] = MatA.randRow(WeightInd[s] / MatA.row);
 	}
-	// push back left i_n
-	size_t *sampled_in = new size_t[num_sample];
-	for (int i = 1; i < nrhs; ++i){
-		size_t sum_k = 0;
-		for (int k = 0; k < RANK; ++k){
-			size_t n = vMatrixs[i]->get_row();
-			double sum_pdf = vMatrixs[i]->sum_of_col()[k];
-			size_t *start_result = sampled_in + sum_k;
-			double *start_pdf = vMatrixs[i]->get_pr() + k*n;
-			vose_alias(freq_k[k],start_result,n,start_pdf,sum_pdf);
-			sum_k += freq_k[k];
-		}
-		for (int s = 0; s < num_sample; ++s){
-			coords[s].push_back(sampled_in[s]);
-		}
+	// sample j;
+	size_t offset = 0;
+	for (int k = 0; k < MatA.row; ++k){
+		vose_alias( freq_k[k], (IndforJ + offset), \
+					MatB.row, \
+					(MatB.element + k*MatB.row), \
+					MatB.SumofCol[k]);
+		offset += freq_k[k];
 	}
-	delete []sampled_in;
-	//compute x
-	for (int s = 0; s < num_sample ; ++s){
-		/* Step 2: Sample coordinate */
-		sgn = 1;
-		/* sample i_0 and k with weight matrix*/
-		index = index_weight[s];
-		/*(i_0,k) sampled*/
-		i_0 = index % L_HEAD;
-		k = index / L_HEAD;
-		sgn *= sgn_foo(vMatrixs[0]->get_element(k,i_0));
-		for(size_t i_n = 1; i_n < nrhs; ++i_n){ 
-			sgn *= sgn_foo(vMatrixs[i_n]->get_element(coords[s][i_n],k));
-		}
-		/* sample kp from the start matrix*/
-		kp = vMatrixs[0]->sample_row_index(i_0);
-		sgn *= sgn_foo(vMatrixs[0]->get_element(i_0,kp));
-		value_of_sampled = 1.0;
-		for (int i = 1; i < nrhs; ++i){
-			value_of_sampled *= vMatrixs[i]->get_element(coords[s][i],kp);
-		}
-		value_of_sampled *= sgn;
-		/* Step 3: Update the element in coordinate(i_0,i_1,...,i_N) */
-		Tensor[ coords[s] ] += value_of_sampled;
+	// compute update value and saved in map<pair, value>
+	double valueSampled = 1.0;
+	size_t indi,indj,indk,indkp;
+	std::map<struct indexIJ, double> IrJc;
+	for (int s = 0; s < NumSample ; ++s){
+		// sample k'
+		indk = WeightInd[s] % MatA.row;
+		indkp = IndforKp[s];
+		indi = IndforI[s];
+		indj = IndforJ[s];
+		valueSampled = 1.0;
+		valueSampled *= sgn_foo(MatA.GetEmelent(indk,indi));
+		valueSampled *= sgn_foo(MatB.GetEmelent(indj,indk));
+		valueSampled *= sgn_foo(MatA.GetEmelent(indkp,indi));
+		valueSampled *= MatB.GetEmelent(indj,indkp);
+		// Update the element in coordinate
+		IrJc[struct indexIJ(indi,indj)] += valueSampled;
 	}
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
 	printf("%f seconds during sampling\n",duration);
-	/* sort value in tensor */
+
+	//-----------------------------------
+	//sort the values have been sampled
+	//-----------------------------------
+
 	start = clock();
-	std::vector<PAIR> pair_vec;
-	std::map<coordinate,double>::iterator map_itr;
+	std::vector<indValue> sortVec;
+	std::map<struct indexIJ, double>::iterator mapItr;
 	double true_value = 0;
 
-	for (map_itr = Tensor.begin(); map_itr != Tensor.end(); ++map_itr){
-
-		true_value = vectors_mul((map_itr->first),vMatrixs,nrhs,RANK);
-		pair_vec.push_back(make_pair(map_itr->first,true_value));
-		//pair_vec.push_back(make_pair(map_itr->first,map_itr->second));
+	for (mapItr = IrJc.begin(); mapItr != IrJc.end(); ++mapItr){
+		true_value =  vectors_mul(mapItr->first,MatA, MatB);
+		sortVec.push_back(std::make_pair(mapItr->first,true_value));
+		//sortVec.push_back(make_pair(mapItr->first,mapItr->second));
 	}
-	sort(pair_vec.begin(),pair_vec.end(),cmp);
-
+	sort(sortVec.begin(),sortVec.end(),cmp);
 
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
-	printf("%f seconds during computer and sorting tenosor \n",duration);
-
+	printf("%f seconds during computer and sorting tensor \n",duration);
+ 
+	//--------------------------------
+	// Converting to Matlab
+	//--------------------------------
 	start = clock();
-	size_t phls_row = pair_vec.size();
-	plhs[0] = mxCreateNumericMatrix(phls_row, nrhs, mxUINT64_CLASS, mxREAL);
+	size_t phls_row = sortVec.size();
+	// pair
+	plhs[0] = mxCreateNumericMatrix(phls_row, 2, mxUINT64_CLASS, mxREAL);
 	uint64_T* plhs_pr = (uint64_T*)mxGetData(plhs[0]);
+	// value
 	plhs[1] = mxCreateDoubleMatrix(phls_row, 1, mxREAL);
 	double *plhs_result = mxGetPr(plhs[1]);
-	for(size_t m = 0; m < pair_vec.size(); ++m){
-		plhs_result[m] = pair_vec[m].second;
-		//plhs_result[m] = vectors_mul(pair_vec[m].first,vMatrixs,nrhs,RANK);
-		for(size_t n = 0; n < nrhs; ++n){
-			/*index start with 1*/
-			plhs_pr[m+n*phls_row] = ((pair_vec[m].first[n])+1);
-		}
+	for(size_t m = 0; m < sortVec.size(); ++m){
+		//value
+		plhs_result[m] = sortVec[m].second;
+		//i
+		plhs_pr[m] = (sortVec[m].first.indI + 1);
+		//j
+		plhs_pr[m + phls_row] = (sortVec[m].first.indJ + 1);
 	}
 
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
 	printf("%f seconds during converting \n",duration);
+	
+	//---------------
+	// free
+	//---------------
+	free(weight);
+	free(WeightInd);
+	free(IndforI);
+	free(IndforJ);
+	free(IndforKp);
+	free(freq_k);
 
-	//display_map(Tensor);
-	delete []weight;
-	delete []index_weight;
-	std::vector<FactorMatrix*>::iterator itr;
-	for (itr = vMatrixs.begin(); itr != vMatrixs.end() ; ++itr){
-		delete *itr;
+}
+
+int sample_index(size_t n, size_t *index, \
+				 size_t row, int *freq_k,\
+				 size_t Range, double*pdf, double sum_pdf){
+	/* get n random numbers*/
+	std::vector<double> ind_u;
+	for (size_t i = 0; i < n; ++i){
+		ind_u.push_back(sum_pdf*((double)rand()/(double)RAND_MAX));
 	}
+	/* sort the random values*/
+	sort(ind_u.begin(),ind_u.end());
+	size_t k = 0;
+	double sum_prob = pdf[0];
+	for (size_t i = 0; i < n; ++i){
+		while((ind_u[i] >= sum_prob) && (k < (Range-1))){
+			sum_prob += pdf[++k];
+		}
+		index[i] = k;
+		freq_k[k % row] ++;
+	}
+	return 1;
+}
+
+/* 
+	Vose's alias method for sample
+*/
+int vose_alias(size_t s, size_t *dst, \
+			   size_t n, double *pdf,double sum_pdf){
+	double *scaled_prob = new double[n];
+	double *table_prob = new double[n];
+	size_t *table_alias = new size_t[n];
+	size_t *table_small = new size_t[n];
+	size_t *table_large = new size_t[n];
+	size_t small_index = 0;
+	size_t large_index = 0;
+	/* stage 1: initialization */
+	for (size_t i = 0; i < n; ++i){
+		scaled_prob[i] = abs(pdf[i]) * n;
+		if ( scaled_prob[i] < sum_pdf ){
+			table_small[small_index] = i;
+			++small_index;
+		}else{
+			table_large[large_index] = i;
+			++large_index;
+		}
+	}
+	size_t l,g;
+	while(small_index != 0 && large_index != 0){
+		small_index -= 1;
+		large_index -= 1;
+		l = table_small[small_index];
+		g = table_large[large_index];
+		table_prob[l] = scaled_prob[l];
+		table_alias[l] = g;
+		scaled_prob[g] = (scaled_prob[g] + scaled_prob[l]) - sum_pdf;
+
+		if (scaled_prob[g] < sum_pdf){
+			table_small[small_index] = g;
+			++small_index;
+		}else{
+			table_large[large_index] = g;
+			++large_index;
+		}
+	}
+	while(large_index != 0){
+		large_index -= 1;
+		table_prob[table_large[large_index]] = sum_pdf;
+	}
+	while(small_index != 0){
+		small_index -= 1;
+		table_prob[table_small[small_index]] = sum_pdf;
+	}
+	/* stage 2: random sampling */
+	double u;
+	size_t fair_die;
+	for (size_t i = 0; i < s; ++i ){
+		fair_die = rand() % n;
+		u = sum_pdf*(double)rand()/(double)RAND_MAX;
+		if (table_prob[fair_die] >= u){
+			dst[i] = fair_die;
+		}else{
+			dst[i] = table_alias[fair_die];
+		}
+	}
+	delete []table_prob;
+	delete []table_alias;
+	delete []scaled_prob;
+	delete []table_small;
+	delete []table_large;
+	return 1;
 }
