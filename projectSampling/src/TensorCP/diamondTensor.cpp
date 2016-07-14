c63b43cbbe644be615b2aa13459dfcb566bca9b0
lzcn <luzhi92@gmail.com>
2016-07-14 10:54:41 +0800

    change the size of output
    small adjustment for annotation  and code

--- projectSampling/src/TensorCP/diamondTensor.cpp
+++ projectSampling/src/TensorCP/diamondTensor.cpp
@@ -49,14 +49,20 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	// Initialization
 	//--------------------
 	start = clock();
+	// input
 	Matrix MatA(mxGetM(prhs[0]),mxGetN(prhs[0]),mxGetPr(prhs[0]));
 	Matrix MatB(mxGetM(prhs[1]),mxGetN(prhs[1]),mxGetPr(prhs[1]));
 	Matrix MatC(mxGetM(prhs[2]),mxGetN(prhs[2]),mxGetPr(prhs[2]));
 	const size_t budget = (size_t)mxGetPr(prhs[3])[0];
 	const size_t NumSample = (size_t)mxGetPr(prhs[4])[0];
 	const size_t top_t = (size_t)mxGetPr(prhs[5])[0];
+	// output
+	plhs[0] = mxCreateDoubleMatrix(top_t, 1, mxREAL);
+	double *plhs_result = mxGetPr(plhs[0]);
 	plhs[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
 	double *tsec = mxGetPr(plhs[1]);
+	plhs[2] = mxCreateNumericMatrix(top_t, 3, mxUINT64_CLASS, mxREAL);
+	uint64_T* plhs_pr = (uint64_T*)mxGetData(plhs[2]);	
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
 	*tsec = duration;
@@ -72,22 +78,22 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	double *weight = (double*)malloc(MatA.row*MatA.col*sizeof(double));
 	memset(weight, 0, MatA.row*MatA.col*sizeof(double));
 	double tempW = 0;
-	// weight[k * MatA.col + i] : i-th column k-th row
-	for (size_t k = 0; k < MatA.row; ++k){
+	// weight[r * MatA.col + i] : i-th column r-th row
+	for (size_t r = 0; r < MatA.row; ++r){
 		for(size_t i = 0; i < MatA.col; ++i){
-			//w_{ki} = |a_{ki}|*||a_{*i}||_1*||b_{*k}||_1
+			//w_{ri} = |a_{ri}|*||a_{*i}||_1*||b_{*r}||_1
 			tempW = 1;
-			tempW *= abs(MatA.GetElement(k,i));
+			tempW *= abs(MatA.GetElement(r,i));
 			tempW *= MatA.SumofCol[i];
-			tempW *= MatB.SumofCol[k];
-			tempW *= MatC.SumofCol[k];
-			weight[k*MatA.col + i] = tempW;
+			tempW *= MatB.SumofCol[r];
+			tempW *= MatC.SumofCol[r];
+			weight[r*MatA.col + i] = tempW;
 			SumofW += tempW;
 		}
 	}
-	for (size_t k = 0; k < MatA.row; ++k){
+	for (size_t r = 0; r < MatA.row; ++r){
 		for(size_t i = 0; i < MatA.col; ++i){
-			weight[k*MatA.col + i] /= SumofW;
+			weight[r*MatA.col + i] /= SumofW;
 		}
 	}
 	finish = clock();
@@ -102,66 +108,65 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	// sampled index  for weight
 	size_t *WeightInd = (size_t *)malloc(NumSample*sizeof(size_t));
 	memset(WeightInd, 0, NumSample*sizeof(size_t));
-	// sampled k, m, n, p, k'
-	size_t *IndforM = (size_t*)malloc(NumSample*sizeof(size_t));
-	memset(IndforM, 0, NumSample*sizeof(size_t));
-	size_t *IndforN = (size_t*)malloc(NumSample*sizeof(size_t));
-	memset(IndforN, 0, NumSample*sizeof(size_t));
-	size_t *IndforP = (size_t*)malloc(NumSample*sizeof(size_t));
-	memset(IndforP, 0, NumSample*sizeof(size_t));
-	size_t *IndforK = (size_t*)malloc(NumSample*sizeof(size_t));
-	memset(IndforK, 0, NumSample*sizeof(size_t));	
-	size_t *IndforKp = (size_t*)malloc(NumSample*sizeof(size_t));
-	memset(IndforKp, 0, NumSample*sizeof(size_t));
-	// sampled k's frequency 
-	size_t *freq_k = (size_t*)malloc(MatA.row*sizeof(size_t));
-	memset(freq_k, 0, MatA.row*sizeof(size_t));
+	// sampled r, m, n, p, r'
+	size_t *IdxI = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IdxI, 0, NumSample*sizeof(size_t));
+	size_t *IdxJ = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IdxJ, 0, NumSample*sizeof(size_t));
+	size_t *IdxK = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IdxK, 0, NumSample*sizeof(size_t));
+	size_t *IdxR = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IdxR, 0, NumSample*sizeof(size_t));	
+	size_t *IdxRp = (size_t*)malloc(NumSample*sizeof(size_t));
+	memset(IdxRp, 0, NumSample*sizeof(size_t));
+	// sampled r's frequency 
+	size_t *freq_r = (size_t*)malloc(MatA.row*sizeof(size_t));
+	memset(freq_r, 0, MatA.row*sizeof(size_t));
 
 	start = clock();
-	// Do sample S pairs (k, i)
+	// Do sample S pairs (r, i)
 	sample_index(NumSample, WeightInd, \
-				 IndforM, IndforK, \
-				 freq_k, \
+				 IdxI, IdxR, \
+				 freq_r, \
 				 MatA.row, MatA.col, \
 				 weight, 1.0);
-	// sample k';
+	// sample r';
 	for (int s = 0; s < NumSample; ++s){
-		IndforKp[s] = MatA.randRow(IndforM[s]);
+		IdxRp[s] = MatA.randRow(IdxI[s]);
 	}
 	// sample n;
 	size_t offset = 0;
-	for (int k = 0; k < MatA.row; ++k){
-		vose_alias( freq_k[k], (IndforN + offset), \
+	for (int r = 0; r < MatA.row; ++r){
+		vose_alias( freq_r[r], (IdxJ + offset), \
 					MatB.row, \
-					(MatB.element + k*MatB.row), \
-					MatB.SumofCol[k]);
-		vose_alias( freq_k[k], (IndforP + offset), \
+					(MatB.element + r*MatB.row), \
+					MatB.SumofCol[r]);
+		vose_alias( freq_r[r], (IdxK + offset), \
 					MatC.row, \
-					(MatC.element + k*MatC.row), \
-					MatC.SumofCol[k]);
-		offset += freq_k[k];		
+					(MatC.element + r*MatC.row), \
+					MatC.SumofCol[r]);
+		offset += freq_r[r];		
 	}
 
 	// compute update value and saved in map<pair, value>
 	double valueSampled = 1.0;
-	size_t idxm, idxn, idxp, idxk, idkp;
 	// use map IrJc to save the sampled values
 	std::map<point3D, double> IrJc;
 	for (int s = 0; s < NumSample ; ++s){
-		idxk = IndforK[s];
-		idkp = IndforKp[s];
-		idxm = IndforM[s];
-		idxn = IndforN[s];
-		idxp = IndforP[s];
+		size_t i = IdxI[s];
+		size_t j = IdxJ[s];
+		size_t k = IdxK[s];
+		size_t r = IdxR[s];
+		size_t rp = IdxRp[s];
 		valueSampled = 1.0;
-		valueSampled *= sgn_foo(MatA.GetElement(idxk,idxm));
-		valueSampled *= sgn_foo(MatB.GetElement(idxn,idxk));
-		valueSampled *= sgn_foo(MatC.GetElement(idxp,idxk));
-		valueSampled *= sgn_foo(MatA.GetElement(idkp,idxm));
-		valueSampled *= MatB.GetElement(idxn,idkp);
-		valueSampled *= MatC.GetElement(idxp,idkp);
+		valueSampled *= sgn_foo(MatA.GetElement(r,i));
+		valueSampled *= sgn_foo(MatB.GetElement(j,r));
+		valueSampled *= sgn_foo(MatC.GetElement(k,r));
+		valueSampled *= sgn_foo(MatA.GetElement(rp,i));
+		valueSampled *= MatB.GetElement(j,rp);
+		valueSampled *= MatC.GetElement(k,rp);
 		// Update the element in coordinate
-		IrJc[point3D(idxm,idxn,idxp)] += valueSampled;
+		IrJc[point3D(i, j, k)] += valueSampled;
 	}
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
@@ -175,11 +180,10 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	std::vector<indValue> tempSortedVec;
 	std::vector<indValue> sortVec;
 	std::map<point3D, double>::iterator mapItr;
-
-	start = clock();
 	for (mapItr = IrJc.begin(); mapItr != IrJc.end(); ++mapItr){
 		tempSortedVec.push_back(std::make_pair(mapItr->first,mapItr->second));
 	}
+	start = clock();
 	sort(tempSortedVec.begin(), tempSortedVec.end(), cmp);
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
@@ -203,24 +207,16 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	// Converting to Matlab
 	//--------------------------------
 	start = clock();
-	size_t phls_row = sortVec.size();
-	// value
-	plhs[0] = mxCreateDoubleMatrix(phls_row, 1, mxREAL);
-	double *plhs_result = mxGetPr(plhs[0]);
-	// pair
-	plhs[2] = mxCreateNumericMatrix(phls_row, 3, mxUINT64_CLASS, mxREAL);
-	uint64_T* plhs_pr = (uint64_T*)mxGetData(plhs[2]);
 	for(size_t m = 0; m < sortVec.size() && m < top_t; ++m){
 		//value
 		plhs_result[m] = sortVec[m].second;
-		//m
+		//i
 		plhs_pr[m] = (sortVec[m].first.x + 1);
-		//n
-		plhs_pr[m + phls_row] = (sortVec[m].first.y + 1);
-		//p
-		plhs_pr[m + phls_row + phls_row] = (sortVec[m].first.z + 1);
+		//j
+		plhs_pr[m + top_t] = (sortVec[m].first.y + 1);
+		//k
+		plhs_pr[m + top_t + top_t] = (sortVec[m].first.z + 1);
 	}
-
 	finish = clock();
 	duration = (double)(finish-start) / CLOCKS_PER_SEC;
 	mexPrintf("%f seconds during converting \n",duration);
@@ -229,11 +225,10 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	//---------------
 	free(weight);
 	free(WeightInd);
-	free(IndforM);
-	free(IndforN);
-	free(IndforP);
-	free(IndforK);
-	free(IndforKp);
-	free(freq_k);
-
+	free(IdxI);
+	free(IdxJ);
+	free(IdxK);
+	free(IdxR);
+	free(IdxRp);
+	free(freq_r);
 }
