5c2c909e086d2c2a7a04bb8ec9e7810f92ae21d0
lzcn <luzhi92@gmail.com>
2016-07-15 19:14:35 +0800

    add indexes and fix the bug
    initialization bugs

--- projectSampling/src/ExactSearch/exactSearchThreeOrderTrensor.cpp
+++ projectSampling/src/ExactSearch/exactSearchThreeOrderTrensor.cpp
@@ -10,14 +10,20 @@
 
 #include <list>
 #include <ctime>
+#include <map>
 
 #include "mex.h"
-#include "../../include/matrix.h"
+#include "matrix.h"
 
+typedef std::pair<point3D,double> indValue;
+
+int cmp(const indValue &x,const indValue&y){
+    return (x.second > y.second);
+}
 
 /*
     all matrices must has the same row dimension
-    [value, time] = exactSearchThreeOrderTrensor(A,B,C,top_t)
+    [value, time, indexes] = exactSearchThreeOrderTrensor(A,B,C,top_t)
 */
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
@@ -32,45 +38,70 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     Matrix C(mxGetM(prhs[2]),mxGetN(prhs[2]),mxGetPr(prhs[2]));
 
     std::list<double> listTop;
+    std::list<point3D> listIdx;
+    std::vector<indValue> tempVec;
 
     const int top_t = mxGetPr(prhs[3])[0];
     double temp = 0.0;
     bool initTop = true;
     int count  = 0;
+    int si,sj,sk;
     for (int i = 0; i < A.col && initTop; ++i){
         for (int j = 0; j < B.col && initTop; ++j){
             for (int k = 0; k < C.col && initTop; ++k){
                 temp = MatrixColMul(A,B,C,i,j,k);
                 if(count < top_t){
-                    listTop.push_back(temp);
+                    tempVec.push_back(std::make_pair(point3D(i,j,k),temp));
                 }else{
                     initTop = false;
+                    si = i;
+                    sj = j;
+                    sk = k;
                 }
                 ++count;
             }
         }
     }
-    listTop.sort(); listTop.reverse();
+    sort(tempVec.begin(),tempVec.end(),cmp);
+    for(auto itr = tempVec.begin(); itr != tempVec.end(); ++itr){
+        listTop.push_back(itr->second);
+        listIdx.push_back(itr->first);
+    }
     start = clock();
-    for(int i = 0; i < A.col; ++i){
-        for(int j = 0; j < B.col; ++j){
-            for(int k = 0; k < C.col; ++k){
+    for(int i = si; i < A.col; ++i){
+        for(int j = sj; j < B.col; ++j){
+            for(int k = sk; k < C.col; ++k){
                 temp = MatrixColMul(A,B,C,i,j,k);
                 if(temp > listTop.back()){
-                    doInsert(temp, listTop);
+                    doInsert(temp, listTop, point3D(i,j,k), listIdx);
                 }
             }
         }
     }
     finish = clock();
     duration = (double)(finish - start)/CLOCKS_PER_SEC;
-    plhs[0] = mxCreateDoubleMatrix(listTop.size(),1,mxREAL);
+    //---------------------------------
+    // convert result to Matlab format
+    //---------------------------------
+    size_t length = listTop.size();
+    plhs[0] = mxCreateDoubleMatrix(length,1,mxREAL);
+    double *topValue = mxGetPr(plhs[0]);
     plhs[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
     mxGetPr(plhs[1])[0] = duration;
-    double *topValue = mxGetPr(plhs[0]);
-    std::list<double>::iterator itr = listTop.begin();
-    for(int i = 0; i < listTop.size(); ++i){
+    plhs[2] = mxCreateNumericMatrix(length, 3, mxUINT64_CLASS, mxREAL);
+    uint64_T* plhs_pr = (uint64_T*)mxGetData(plhs[2]);  
+    auto itr = listTop.begin();
+    auto itr2 = listIdx.begin();
+    for(int i = 0; i < length; ++i){
+        // value
         topValue[i] = (*itr);
+        // indexes
+        plhs_pr[i] = ((*itr2).x + 1);
+        //j
+        plhs_pr[i + length] = ((*itr2).y + 1);
+        //k
+        plhs_pr[i + length + length] = ((*itr2).z + 1);
         ++itr;
+        ++itr2;
     }
 }
