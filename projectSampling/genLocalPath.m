b26e67966ef316833ca1091dbadbb53ab86a6463
lzcn <luzhi92@gmail.com>
2016-07-20 17:21:32 +0800

    local path

--- projectSampling/genLocalPath.m
+++ projectSampling/genLocalPath.m
@@ -2,7 +2,5 @@ path = pwd;
 ipath = fullfile(path,'include');
 src_path = fullfile(path,'src/');
 bin_path = fullfile(path,'bin');
-addpath(bin_path);
 data_path = fullfile(path,'data');
-bin_path = fullfile(path,'bin');
 addpath(bin_path);
