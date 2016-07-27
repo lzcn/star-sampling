7b733200429692d05a72afe7e604ceee4ce3607a
Zhi Lu <luzhi92@gmail.com>
2016-07-27 20:14:50 +0800

    MODIFY function name

--- projectSampling/experiments/TRIAL004/RUN_TRIAL004.m
+++ projectSampling/experiments/TRIAL004/RUN_TRIAL004.m
@@ -14,11 +14,11 @@ top_t = budget(1);
 % number of samples
 samples = budget(end);
 % times of experiments
-turn = 10;
+turn = 1;
 % folder to save result
 out_dir = './result';
 if(~isdir(out_dir))
     mkdir(out_dir);
 end
 
-ex004(data_path,out_dir,samples,budget,top_t,turn);
+TRIAL004(data_path,out_dir,samples,budget,top_t,turn);
