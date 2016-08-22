9378a6bddda7807bbf066465a1ba78155b63f6e7
Zhi Lu <luzhi92@gmail.com>
2016-08-22 16:33:06 +0800

    replace sizet_t by  uint , use  set STL

--- projectSampling/include/dmatrix.h
+++ projectSampling/include/dmatrix.h
@@ -29,18 +29,18 @@ struct SVector {
 class DMatrix{
 public:    
     DMatrix();
-    DMatrix(size_t r_dim, size_t c_dim);
-    DMatrix(size_t r_dim, size_t c_dim, double *ptr);
+    DMatrix(uint r_dim, uint c_dim);
+    DMatrix(uint r_dim, uint c_dim, double *ptr);
 	~DMatrix();
-    double& operator()(size_t i, size_t j);
-	double operator()(size_t i, size_t j)const;
-	size_t randRow(size_t n, double sum);
-	size_t randCol(size_t m, double sum);
+    double& operator()(uint i, uint j);
+	double operator()(uint i, uint j)const;
+	uint randRow(uint n, double sum);
+	uint randCol(uint m, double sum);
     void init(uint TYPE, double mean, double stdev);
     double* get_abs_row_sum();
     double* get_abs_col_sum();
 public:
-	size_t row, col;
+	uint row, col;
 	double *value;
 private:
     bool SELFINITVALUE;
@@ -50,14 +50,14 @@ private:
 class DVector{
 public:
     DVector();
-    DVector(size_t v_dim);
-    DVector(size_t v_dim, double*ptr);
+    DVector(uint v_dim);
+    DVector(uint v_dim, double*ptr);
     void init(uint TYPE, double mean, double stdev);
 	~DVector();
-    double& operator()(size_t n);
-	double operator()(size_t n)const;
+    double& operator()(uint n);
+	double operator()(uint n)const;
 public:
-	size_t dim;
+	uint dim;
 	double* value;
 private:
     bool SELFINITVALUE;
@@ -75,13 +75,13 @@ DMatrix::DMatrix(){
     value = nullptr;
     SELFINITVALUE = false;
 }
-DMatrix::DMatrix(size_t r_dim, size_t c_dim){
+DMatrix::DMatrix(uint r_dim, uint c_dim){
     row = r_dim;
     col = c_dim;
     value = nullptr;
     SELFINITVALUE = false;
 }
-DMatrix::DMatrix(size_t r_dim, size_t c_dim, double *ptr){
+DMatrix::DMatrix(uint r_dim, uint c_dim, double *ptr){
     row = r_dim;
     col = c_dim;
     value = ptr;
@@ -94,12 +94,12 @@ void DMatrix::init(uint TYPE, double mean, double stdev){
     }
     switch (TYPE) {
         case INIT_RAND_N:
-            for(size_t i = 0; i < col*row; ++i){
+            for(uint i = 0; i < col*row; ++i){
                 // normal distribution with \mu = mean,\sigma^2 = stdev
                 value[i] = rnd::randn(mean, stdev);
             }break;
         case INIT_RAND_U:
-            for(size_t i = 0; i < col*row; ++i)
+            for(uint i = 0; i < col*row; ++i)
                 value[i] = rnd::randu();
             break;
         case INIT_ALL_ONES:
@@ -111,18 +111,18 @@ void DMatrix::init(uint TYPE, double mean, double stdev){
         default: break;
     }
 }
-double& DMatrix::operator()(size_t i, size_t j){ 
+double& DMatrix::operator()(uint i, uint j){ 
     //assert((i >= row) && (j >= col));
     return value[j*row +i];
 }
-double DMatrix::operator()(size_t i, size_t j) const {
+double DMatrix::operator()(uint i, uint j) const {
     return value[j*row +i];
 }
-size_t DMatrix::randRow(size_t n, double sum){
+uint DMatrix::randRow(uint n, double sum){
     double x,temp;
     x = sum*((double)rand()/(double)RAND_MAX);
     temp = 0;
-    for (size_t i = 0; i < row; ++i){
+    for (uint i = 0; i < row; ++i){
         temp += abs(value[i + n*row]);
         if(x <= temp){ 
             return i;
@@ -130,11 +130,11 @@ size_t DMatrix::randRow(size_t n, double sum){
     }
     return (row-1);
 }
-size_t DMatrix::randCol(size_t m, double sum){
+uint DMatrix::randCol(uint m, double sum){
     double x,temp;
     x = sum*((double)rand()/(double)RAND_MAX);
     temp = 0;
-    for (size_t j = 0; j < col; ++j){
+    for (uint j = 0; j < col; ++j){
         temp += abs(value[m + j*row]);
         if(x <= temp){ 
             return j;
@@ -148,21 +148,21 @@ DVector::DVector(){
     value = nullptr;
     SELFINITVALUE = false;
 }
-DVector::DVector(size_t v_dim){
+DVector::DVector(uint v_dim){
     dim = v_dim;
     value = nullptr;
     SELFINITVALUE = false;
 }
-DVector::DVector(size_t v_dim, double *ptr){
+DVector::DVector(uint v_dim, double *ptr){
     dim = v_dim;
     value = ptr;
     SELFINITVALUE = false;
 }
-double& DVector::operator()(size_t n){ 
+double& DVector::operator()(uint n){ 
     //assert((i >= row) && (j >= col));
     return value[n];
 }
-double DVector::operator()(size_t n) const {
+double DVector::operator()(uint n) const {
     return value[n];
 }
 
@@ -173,12 +173,12 @@ void DVector::init(uint TYPE, double mean, double stdev){
     }
     switch (TYPE) {
         case INIT_RAND_N:
-            for(size_t i = 0; i < dim; ++i)
+            for(uint i = 0; i < dim; ++i)
                 // normal distribution with \mu = mean,\sigma^2 = stdev
                 value[i] = rnd::randn(mean, stdev);
             break;
         case INIT_RAND_U:
-            for(size_t i = 0; i < dim; ++i)
+            for(uint i = 0; i < dim; ++i)
                 value[i] = rnd::randu();
             break;
         case INIT_ALL_ONES:
--- projectSampling/src/factorization.cpp
+++ projectSampling/src/factorization.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <set>
 #include <cstdio>
 #include <ctime>
 #include "dmatrix.h"
@@ -6,17 +7,17 @@
 #include "coordinate.h"
 #include "random.h"
 #include "mex.h"
-double diff_y_pred(DMatrix &U,DMatrix &I,DMatrix &T,size_t u,size_t i,size_t pos,size_t neg){
-	size_t rank = U.row;
+double diff_y_pred(DMatrix &U,DMatrix &I,DMatrix &T,uint u,uint i,uint pos,uint neg){
+	uint rank = U.row;
 	double ans = 0.0;
-	for(size_t r = 0; r < rank; ++r){
+	for(uint r = 0; r < rank; ++r){
 		ans += U(r,u)*I(r,i)*(T(r,pos)-T(r,neg));
 	}
 	return ans;
 }
 double Frobenius(DMatrix &A){
 	double ans = 0.0;
-	for(size_t i = 0; i < A.col*A.row; i++){
+	for(uint i = 0; i < A.col*A.row; i++){
 		ans += A.value[i]*A.value[i];
 	}
 	return sqrt(ans);
@@ -27,40 +28,40 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     // Remove some tuple untill to be 5-core data
     //--------------------------------------------
 	clock_t start;
-	size_t NumofEntry = mxGetM(prhs[0]);
-	size_t *userid = (size_t*)malloc(NumofEntry*sizeof(size_t));
-	size_t *itemid = (size_t*)malloc(NumofEntry*sizeof(size_t));
-    size_t *tagid  = (size_t*)malloc(NumofEntry*sizeof(size_t));
-	for (size_t i = 0; i < NumofEntry; ++i){
-		userid[i] = (size_t)mxGetPr(prhs[0])[i];
-		itemid[i] = (size_t)mxGetPr(prhs[1])[i];
-		tagid[i]  = (size_t)mxGetPr(prhs[2])[i];
+	uint NumofEntry = mxGetM(prhs[0]);
+	uint *userid = (uint*)malloc(NumofEntry*sizeof(uint));
+	uint *itemid = (uint*)malloc(NumofEntry*sizeof(uint));
+    uint *tagid  = (uint*)malloc(NumofEntry*sizeof(uint));
+	for (uint i = 0; i < NumofEntry; ++i){
+		userid[i] = (uint)mxGetPr(prhs[0])[i];
+		itemid[i] = (uint)mxGetPr(prhs[1])[i];
+		tagid[i]  = (uint)mxGetPr(prhs[2])[i];
 	}
     // record the data to a map, and count user,item,tag
-	std::map<point3D, bool> UserItemTag;
-	std::map<size_t, int> UserCount;
-	std::map<size_t, int> ItemCount;
-	std::map<size_t, int> TagCount;
+	std::set<point3D> UserItemTag;
+	std::map<uint, int> UserCount;
+	std::map<uint, int> ItemCount;
+	std::map<uint, int> TagCount;
 	for(auto i = 0; i < NumofEntry; ++i){
-        UserItemTag[point3D(userid[i],itemid[i],tagid[i])] = true;
+        UserItemTag.insert( point3D(userid[i],itemid[i],tagid[i]) );
         UserCount[userid[i]] += 1;
         ItemCount[itemid[i]] += 1;
         TagCount[ tagid[i] ] += 1;
 	}
 	// do delete
 	for(;;){
-		std::map<point3D,bool> temp;
+		std::set<point3D> temp;
         temp = UserItemTag;
 		bool changed = false;
-		mexPrintf("Current length of tuple:%d\n", temp.size());
+		mexPrintf("Current length of tuple:%d\n", temp.size());mexEvalString("drawnow");
 		for(auto itr = temp.begin(); itr != temp.end(); ++itr){
-			if(UserCount[itr->first.x] < 5 || ItemCount[itr->first.y] < 5 || TagCount[itr->first.z] < 5){
+			if(UserCount[itr->x] < 5 || ItemCount[itr->y] < 5 || TagCount[itr->z] < 5){
 				// delete the tuple
-				UserItemTag.erase(itr->first);
+				UserItemTag.erase(*itr);
 				// decrement count for <user, movie, tag>
-				--UserCount[itr->first.x];
-				--ItemCount[itr->first.y];
-				--TagCount[itr->first.z];
+				--UserCount[itr->x];
+				--ItemCount[itr->y];
+				--TagCount[itr->z];
 				// switch the state to changed
 				changed = true;
 			}
@@ -71,17 +72,17 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	}
     // comprese the range of user, item, tag from 0 to the size;
 	// compression map
-	std::map<size_t,size_t> UserMap;
-	std::map<size_t,size_t> ItemMap;
-	std::map<size_t,size_t> TagMap;
+	std::map<uint,uint> UserMap;
+	std::map<uint,uint> ItemMap;
+	std::map<uint,uint> TagMap;
 	// find the ID has occurred
 	for(auto itr = UserItemTag.begin(); itr != UserItemTag.end(); ++itr){
-		UserMap[itr->first.x] = 1;
-		ItemMap[itr->first.y] = 1;
-		TagMap[itr->first.z] = 1;
+		UserMap[itr->x] = 1;
+		ItemMap[itr->y] = 1;
+		TagMap[itr->z] = 1;
 	}
 	// record the compression map
-	size_t offset = 0;
+	uint offset = 0;
 	for(auto itr = UserMap.begin(); itr != UserMap.end(); ++itr){
 		itr->second = offset++;
 	}
@@ -94,30 +95,30 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 		itr->second = offset++;
 	}
     // map to save <posts, posTag>
-	typedef std::vector<size_t> posTag;
+	typedef std::vector<uint> posTag;
     std::map<point2D, posTag> Posts;
 	
 	for(auto itr = UserItemTag.begin(); itr != UserItemTag.end(); ++itr){
-        size_t user = UserMap[itr->first.x];
-        size_t item = ItemMap[itr->first.y];
-        size_t tag  = TagMap[itr->first.z];
+        uint user = UserMap[itr->x];
+        uint item = ItemMap[itr->y];
+        uint tag  = TagMap[itr->z];
         Posts[point2D(user,item)].push_back(tag);
 	}
-	mexPrintf("Posts size :%d\n",Posts.size());
-
+	mexPrintf("Posts size :%d\n",Posts.size());mexEvalString("drawnow");
+	return;
     //////////////////////////////
     // SGD for factorization
     //////////////////////////////
     // initialized the factor matrices
-    size_t userSize = UserMap.size();
-    size_t itemSize = ItemMap.size();
-    size_t tagSize = TagMap.size();
-    size_t factorSize = 32;
+    uint userSize = UserMap.size();
+    uint itemSize = ItemMap.size();
+    uint tagSize = TagMap.size();
+    uint factorSize = 32;
     double mean = 0.25;
     double stdev = 1e-2;
     double lambda = 1e-6;
     double alpha = 0.1;
-    size_t maxiter = 100;
+    uint maxiter = 1;
 	plhs[0] = mxCreateDoubleMatrix(factorSize, userSize, mxREAL);
 	plhs[1] = mxCreateDoubleMatrix(factorSize, itemSize, mxREAL);
 	plhs[2] = mxCreateDoubleMatrix(factorSize, tagSize, mxREAL);
@@ -130,54 +131,55 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	// SGD method to update factor matrices
 	double *inter_v = (double*)malloc(factorSize*sizeof(double));
 	double *inter_q = (double*)malloc(factorSize*sizeof(double));
-    for(size_t l = 0; l < maxiter;++l){
-		double AUC = 0.0;
+    for(uint l = 0; l < maxiter;++l){
 		//for (u,i) in Posts do
 		start = clock();
 		std::map<point2D,double> wtptn;
+		uint u,i;
+		double AUC = 0.0;
         for(auto itr = Posts.begin();itr != Posts.end(); ++itr,++offset){
 			double AUCui = 0.0;
-            size_t u = itr->first.x;
-            size_t i = itr->first.y;
+            u = itr->first.x;
+            i = itr->first.y;
 			posTag ptagVec = itr->second;
 			auto tag_pos = ptagVec.begin();
 			// compute the intermediate vector v
 			memset(inter_v, 0, factorSize*sizeof(double));
 			for (tag_pos = ptagVec.begin(); tag_pos != ptagVec.end(); ++tag_pos) {
-				for (size_t tag_neg = 0; tag_neg < tagSize; ++tag_neg){
+				for (uint tag_neg = 0; tag_neg < tagSize; ++tag_neg){
 					if(ptagVec.end() != std::find(ptagVec.begin(),ptagVec.end(),tag_neg)){ continue; }
 					double y = diff_y_pred(MatUser,MatItem,MatTag,u,i,*tag_pos,tag_neg);
 					double temp = sigmoid(y)*(1-sigmoid(y));
 					AUCui += sigmoid(y);
 					wtptn[point2D(*tag_pos,tag_neg)] = temp;
-					for(size_t f = 0; f < factorSize; ++f){
+					for(uint f = 0; f < factorSize; ++f){
 						inter_v[f] += temp*(MatTag(f,*tag_pos)-MatTag(f,tag_neg));
 					}
 				}
 			}
 			// compute the intermediate vector q
-			for(size_t f = 0; f < factorSize; ++f){
+			for(uint f = 0; f < factorSize; ++f){
 				inter_q[f] = MatUser(f,u)*MatItem(f,i);
 			}
 			double gradu = 0.0, gradi = 0.0;
 			double z = 1.0 / (double)( ptagVec.size() * (tagSize-ptagVec.size()) );
-			for(size_t f = 0; f < factorSize; ++f){
+			for(uint f = 0; f < factorSize; ++f){
 				gradu += MatItem(f,i)*inter_v[f];
-				gradu += MatUser(f,u)*inter_v[f];
+				gradi += MatUser(f,u)*inter_v[f];
 			}
 			gradu *= z;
 			gradi *= z;
 			AUCui *= z;
             // update user and item matrices
-            for(size_t r = 0; r < factorSize; ++r){
+            for(uint r = 0; r < factorSize; ++r){
                 MatUser(r,u) += alpha*(gradu - lambda*MatUser(r,u));
                 MatItem(r,i) += alpha*(gradi - lambda*MatItem(r,i));
             }
 			// update tag matrix
-            for(size_t t = 0; t < tagSize; ++t){
+            for(uint t = 0; t < tagSize; ++t){
 				double gradt = 0.0;
 				if(ptagVec.end() != std::find(ptagVec.begin(),ptagVec.end(),t)){
-					for(size_t tag_neg = 0; tag_neg < tagSize; ++tag_neg){
+					for(uint tag_neg = 0; tag_neg < tagSize; ++tag_neg){
 						if(ptagVec.end() != std::find(ptagVec.begin(),ptagVec.end(),tag_neg)){ continue; }
 						gradt += wtptn[point2D(t,tag_neg)];
 					}
@@ -188,31 +190,29 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 					}
 					gradt = -1.0 * z * gradt;
 				}
-                for(size_t f = 0; f < factorSize; ++f){
+                for(uint f = 0; f < factorSize; ++f){
 					MatTag(f,t)  += alpha*(inter_q[f]*gradt - lambda*MatTag(f,t));
                 }
             }
 			AUC += AUCui;
         }
-		if(0 == (l % 20)){
-			double regularTerm = 0.0;
-			regularTerm += Frobenius(MatUser);
-			regularTerm += Frobenius(MatItem);
-			regularTerm += Frobenius(MatTag);
-			regularTerm *= lambda;
-			mexPrintf("Iter:%d,ObjectFuntcionValue:%f,timeDuration%f\n",l,AUC-regularTerm,timeDuration(start));
-		}
+		double regularTerm = 0.0;
+		regularTerm += Frobenius(MatUser);
+		regularTerm += Frobenius(MatItem);
+		regularTerm += Frobenius(MatTag);
+		regularTerm *= lambda;
+		mexPrintf("Iter:%d, ObjectFuntcionValue:%f, TimeDuration:%f\n",l,AUC-regularTerm,timeDuration(start));mexEvalString("drawnow");
     }
 	///////////////////
 	// Conveter
 	//////////////////
-	for(size_t i = 0; i < userSize*factorSize; ++i){
+	for(uint i = 0; i < userSize*factorSize; ++i){
 		mxGetPr(plhs[0])[i] = MatUser.value[i];
 	}
-	for(size_t i = 0; i < itemSize*factorSize; ++i){
+	for(uint i = 0; i < itemSize*factorSize; ++i){
 		mxGetPr(plhs[1])[i] = MatItem.value[i];
 	}
-	for(size_t i = 0; i < userSize*factorSize; ++i){
+	for(uint i = 0; i < userSize*factorSize; ++i){
 		mxGetPr(plhs[2])[i] = MatTag.value[i];
 	}
 	//--------------
